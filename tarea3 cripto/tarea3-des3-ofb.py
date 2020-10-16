253603441f42118f4f4c1b2ebb658bdc418addd9
Jun <65258516+coni-zv@users.noreply.github.com>
2020-10-16 18:06:34 -0300

    Add files via upload

--- tarea3 cripto/tarea3-des3-ofb.py
+++ tarea3 cripto/tarea3-des3-ofb.py
@@ -1,42 +1,10 @@
 from Crypto.Cipher import DES3
 from Crypto import Random
-from Crypto.Util.Padding import pad
-#from Crypto.Util.Padding import unpad
 from base64 import b64encode, b64decode
-import binascii
 
-def pad(data):
-    length = 16 - (len(data) % 16)
-    data = data.decode() + chr(length)*length
-    return data.encode()
-
-def unpad(data):
-    return data[:-ord(data[-1])]
-
-'''
-def encrypt(message, passphrase):
-    IV = Random.new().read(BLOCK_SIZE)
-    cipher_enc = DES3.new(passphrase, DES3.MODE_OFB, IV)
-    return b64encode(IV + cipher_enc.encrypt(pad(message)))
-
-def decrypt(enc,key):
-    iv = enc[:BLOCK_SIZE]
-    cipher_dec = DES3.new(key,DES3.MODE_OFB,iv)
-    dec = cipher_dec.decrypt(enc)
-    return unpad(b64decode(dec[BLOCK_SIZE:]))
-'''
-'''
-def encrypt(msg,key):
-    iv = Random.new().read(BLOCK_SIZE)
-    cipher_enc = DES3.new(key,DES3.MODE_OFB,iv)
-    enc = cipher_enc.encrypt(pad(text))
-    return iv+enc
-'''
-def encrypt(message, passphrase):
-    IV = Random.new().read(BLOCK_SIZE)
+def encrypt(message, passphrase,IV):
     cipher_enc = DES3.new(passphrase, DES3.MODE_OFB, IV)
     return b64encode(IV + cipher_enc.encrypt(message))
-
 
 def decrypt(encrypted, passphrase):
     encrypted = b64decode(encrypted)
@@ -47,57 +15,35 @@ def decrypt(encrypted, passphrase):
 def programa():
     msg = input("Ingrese mensaje a encriptar: ")
     key = input("Ingrese llave (debe tener 16 o 24 caracteres!): ")
-    if len(key)==16 or len(key)==24:
+    iv = input("Ingrese IV (debe tener 8 caracteres!): ")
+    if (len(key)==16 or len(key)==24) and len(iv)==8:
         msg = msg.encode()
         key = key.encode()
-        e = encrypt(msg,key)
-        create_html(e,key)
+        iv = iv.encode()
+        e = encrypt(msg,key,iv)
+        create_html(e,key,iv)
         print("mensaje encriptado: ",e)
+        de = decrypt(e,key).decode()
+        print("mensaje desencriptado: ",de)
         return e
     else:
         print(len(key))
-        print("Error: ingrese llave de nuevo")
+        print("Error: cantidad de caracteres incorrecta")
+        print("largo key: ",len(key))
+        print("largo iv: ",len(iv))
         programa()
 
 
-def create_html(enc,key):
+def create_html(enc,key,iv):
     text = """
     <p>Este sitio contiene un mensaje secreto</p>
-    <div class="tripledes" id='"""+enc.decode()+"""'></div>
-    <div class="key" id='"""+key.decode()+"""'></div>
+    <div class="tripledes" id='"""+str(enc.decode())+"""'></div>
+    <div class="iv" id='"""+str(iv.decode())+"""'></div>
+    <div class="key" id='"""+str(key.decode())+"""'></div>
     <div class="mensaje" id="mensaje"></div>    """
     with open(r"archivo_tarea.html", 'w') as html:
         html.write(text)
 
-
 BLOCK_SIZE=8
-'''
-key = b'sixteen byte key'
-text = b"que tengas un buen dia! "
-e = encrypt(text,key)
-hex_e =binascii.hexlify(e).upper()
-hex_key=binascii.hexlify(key).upper()
-b64_e = b64encode(e)
-de=decrypt(e,key)
-print("encrypt:")
-print(e)
-print("hex enc:")
-print(hex_e)
-print("b64 enc:")
-print(b64_e)
-print("hex key:")
-print(hex_key)
-#print("dec:")
-#print(de)
-'''
-'''
-text = """
-<p>Este sitio contiene un mensaje secreto</p>
-<div class="3DES" id='"""+e.decode()+"""'></div>
-<div class="key" id='"""+key.decode()+"""'></div>
-<div class="hex_key" id='"""+hex_key.decode()+"""'></div>
-<div class="mensaje" id="mensaje"></div>
-"""
-with open(r"archivo3.html", 'w') as html:
-    html.write(text)
-'''
+programa()
+
